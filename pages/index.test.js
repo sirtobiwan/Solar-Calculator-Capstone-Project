45466fb79c6707fb3217b1996c2bbdf331f34eb6
sirtobiwan <125971942+sirtobiwan@users.noreply.github.com>
2023-04-25 05:53:55 -0700

    Delete index.test.js

--- pages/index.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import HomePage from "../pages/index";
-
-test("renders next greeting headline", () => {
-  render(<HomePage />);
-  const linkElement = screen.getByText(/hello from next.js/i);
-  expect(linkElement).toBeInTheDocument();
-});
