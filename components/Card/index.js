e135af92fba44b7deddfc8559a75e85395c25750
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-04-27 10:40:32 +0200

    display-ghi-information

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import Modal from "@/components/Modal";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
 
 const StyledCard = styled.article`
   background-color: #f9f9f9;
@@ -9,6 +13,7 @@ const StyledCard = styled.article`
   margin: 2rem auto;
   max-width: 800px;
   overflow: hidden;
+  position: relative;
 `;
 
 const StyledLocation = styled.div`
@@ -30,6 +35,49 @@ const StyledData = styled.p`
   font-size: 2rem;
   font-weight: bold;
   padding-top: 0.5rem;
+  position: relative;
+`;
+
+const StyledRadiation = styled.span`
+  font-size: 0.8rem;
+  position: absolute;
+  bottom: 0rem;
+  right: 0.2rem;
+  background-color: #f5a623;
+  border-radius: 5px;
+  color: white;
+  padding: 0.25rem 0.5rem;
+  transition: background-color 0.3s ease;
+  &:hover {
+  background-color: #e6951d;
+  border: none;
+  padding: 0.4rem 0.65rem;
+  cursor: pointer;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+`
+  display: inline-block;
+  background-color: #f5a623;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 1.2rem;
+  padding: 0.75rem 1.5rem;
+  cursor: pointer;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s ease;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #e6951d;
+  }
+`;
+
+const StyledImage = styled.img`
+  margin-left: 1rem;
+  width: 70%;
+  height: 70%;
 `;
 
 export default function Card() {
@@ -42,6 +90,27 @@ export default function Card() {
       <StyledLocation>
         <StyledLabel>Annual Radiation</StyledLabel>
         <StyledData>200 kWh/m²</StyledData>
+        <StyledRadiation>
+          <Modal
+            text={
+              <>
+                <FontAwesomeIcon icon={faAngleDown} /> radiation in ghi
+               </>
+                }
+            modalContent={
+              <>
+                <h2>Radiation in GHI </h2>
+                <p>
+                  Global Horizontal Irradiance (GHI) is the total solar
+                  radiation incident on a horizontal surface. It is the sum of
+                  Direct Normal Irradiance (DNI), Diffuse Horizontal Irradiance,
+                  and ground-reflected radiation.
+                </p>
+                <StyledImage src="/ghi.gif" alt="description" />
+              </>
+            }
+          />
+        </StyledRadiation>
       </StyledLocation>
     </StyledCard>
   );
