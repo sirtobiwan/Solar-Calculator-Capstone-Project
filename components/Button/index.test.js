499a5c41742739037632082cc87f0141420b7494
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-05-22 16:22:38 +0200

    update

--- components/Button/index.test.js
+++ components/Button/index.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Button from '../Button';
 
-test('rendert den Button mit dem korrekten Link und Text', () => {
+test('renders the button with the correct link and text', () => {
   const href = 'http://localhost:3000/savings';
   const text = 'next';
 
